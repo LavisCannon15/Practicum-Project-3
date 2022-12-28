b29aa0871c0e167254b955fd7b8c7b22e9507162
LavisCannon15 <85091518+LavisCannon15@users.noreply.github.com>
2022-12-28 17:13:10 -0500

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,32 +1,51 @@
+const yosemiteValleyImage = new URL(
+  "../images/yosemite-valley.jpg",
+  import.meta.url
+);
+const lakeLouiseImage = new URL("../images/lake-louise.jpg", import.meta.url);
+const baldMountainImage = new URL(
+  "../images/bald-mountains.jpg",
+  import.meta.url
+);
+const latemarImage = new URL("../images/latemar.jpg", import.meta.url);
+const vanoiseNationalParkImage = new URL(
+  "../images/vanoise-national-park.jpg",
+  import.meta.url
+);
+const lagoDiBraiesImage = new URL(
+  "../images/lago-di-braies.jpg",
+  import.meta.url
+);
+
 export const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "./images/yosemite-valley.jpg",
+    link: yosemiteValleyImage,
   },
 
   {
     name: "Lake Louise",
-    link: "./images/lake-louise.jpg",
+    link: lakeLouiseImage,
   },
 
   {
     name: "Bald Mountains",
-    link: "./images/bald-mountains.jpg",
+    link: baldMountainImage,
   },
 
   {
     name: "Latemar",
-    link: "./images/latemar.jpg",
+    link: latemarImage,
   },
 
   {
     name: "Vanoise National Park",
-    link: "./images/vanoise-national-park.jpg",
+    link: vanoiseNationalParkImage,
   },
 
   {
     name: "Lago di Braies",
-    link: "./images/lago-di-braies.jpg",
+    link: lagoDiBraiesImage,
   },
 ];
 
